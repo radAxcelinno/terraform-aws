fa58f1b84c24f3bbe3a045d63b90560df25dcd6a
Rad Wadud <rad.wadud@axcelinno.io>
2020-10-26 16:00:33 -0500

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "public_dns" {
   description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.vm.public_dns
+  value       = aws_instance.vm.*.public_dns
 }
