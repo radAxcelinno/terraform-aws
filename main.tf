5d747c532c164962abcb500d55aac3f646b6b636
Rad Wadud <rad.wadud@axcelinno.io>
2020-10-25 21:20:11 -0500

    adde sg as a list

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "vm" {
   ami = "ami-02f147dfb8be58a10"
   instance_type = "t2.micro"
-  security_groups = "sg-0f515b7d62ffa5499"
+  security_groups = ["sg-0f515b7d62ffa5499"]
   user_data = "${file("apache-start.sh")}"
 
 
