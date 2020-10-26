f06f467f17becbdc83a4e64a1dbb2895a5375af8
Rad Wadud <rad.wadud@axcelinno.io>
2020-10-26 16:01:43 -0500

    deleted output

--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "public_dns" {
-  description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.vm.*.public_dns
-}
